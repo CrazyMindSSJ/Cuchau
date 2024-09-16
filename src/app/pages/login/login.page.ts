bce7086b84ed8889bde8faf085bdb67a4e2c67c6
betzuki <bet.ruiz@duocuc.cl>
2024-09-16 19:01:51 -0300

    Cambio de colores de registro y login y algo del menu

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -7,32 +7,50 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+//aquí podemos crear variables, constrantes, listas, arreglos, json, etc:
+  //NOMBRE_VARIABLE: TIPO_DATO = VALOR;
+
+  titulo: string = "PÁGINA DE LOGIN";
+  numero: number = 5;
+  decimal: number = 5.2;
+  existe: boolean = true;
+  fecha_hoy: Date = new Date();
+  nombres: string[] = ["Pedro","Juan","Diego"];
+  persona: any = {"nombre":"Jeison", "edad": 5};
+
+
+ //NgModel:
+ email: string = "";
+ password: string = "";
+
+ //Variables para mostrar la contraseña
+ tipoContrasenia: string = 'password';  
+ iconoContrasenia: string = 'eye';  
 
   constructor(private router: Router) { }
 
   ngOnInit() {
   }
   
-  public alertButtons = ['OK'];
-  public alertInputs = [
-    {
-      placeholder: 'Name',
-    },
-    {
-      placeholder: 'Nickname (max 8 characters)',
-      attributes: {
-        maxlength: 8,
-      },
-    },
-    {
-      type: 'number',
-      placeholder: 'Age',
-      min: 1,
-      max: 100,
-    },
-    {
-      type: 'textarea',
-      placeholder: 'A little about yourself',
-    },
-  ];
+  //método asociado al boton para hacer un login:
+  login(){
+    if(this.email=="" && this.password==""){
+      this.router.navigate(['/home']);
+    }else{
+      alert("CORREO O CONTRASEÑA INCORRECTOS!");
+    }
+  }
+
+
+  mostrarClave() {
+    if (this.tipoContrasenia === 'password') {
+      this.tipoContrasenia = 'text';       
+      this.iconoContrasenia = 'eye-off';  
+    } else {
+      this.tipoContrasenia = 'password';   
+      this.iconoContrasenia = 'eye';       
+    }
+  }
+
 }
+
---
+++ src/app/pages/registro/registro.page.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { min } from 'rxjs';
+
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.page.html',
+  styleUrls: ['./registro.page.scss'],
+})
+export class RegistroPage implements OnInit {
+
+  //Crear variables:
+  //NOMBRE: TIPO = VALOR;
+  //NOMBRE = Clase(VALOR);
+
+  persona = new FormGroup({
+    rut: new FormControl('',[Validators.required,Validators.pattern("[0-9]{7,8}-[0-9kK]{1}")]),
+    nombre: new FormControl('',[Validators.required,Validators.pattern("[a-z]{3,5}")]),
+    fecha_nacimiento: new FormControl('',[Validators.required]),
+    genero: new FormControl('',[Validators.required]),
+    tiene_vehiculo: new FormControl('no',[Validators.required]),
+    patente_vehiculo: new FormControl('',[])
+  });
+
+    public alertButtons = [
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      handler: () => {
+        console.log('Alert canceled');
+      },
+    },
+    {
+      text: 'OK',
+      role: 'confirm',
+      handler: () => {
+        console.log('Alert confirmed');
+      },
+    },
+  ];
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+//podemos crear métodos:
+public registrar():void{
+  //validaciones? llamar un DAO? conversión?
+  console.log(this.persona.value);
+  //alert("registrado!");
+  this.router.navigate(['/login']);
+}
+
+
+
+  setResult(ev:any) {
+    console.log(`Dismissed with role: ${ev.detail.role}`);
+  }
+}
